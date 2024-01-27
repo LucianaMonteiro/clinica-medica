ae1932b56bc03173d668939e71040fe84eb94463
Luciana Monteiro Lima <dra.luciana.lima@gmail.com>
2024-01-27 14:03:44 -0300

    Adicionar biblioteca HTMX para funcoes AJAX

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from fastapi.responses import RedirectResponse, HTMLResponse
 app = FastAPI()
 app.mount("/app", StaticFiles(directory="static", html="True"), name="static")
 
+
 @app.get("/", response_class=RedirectResponse)
 async def root():
     return "/app/login.html"
+
 
 @app.get("/api", response_class=HTMLResponse)
 async def api():
@@ -19,3 +21,26 @@ async def api():
              </head>
         </html>
     """
+
+
+@app.get("/api/medicos")
+async def medicos():
+    return get_medicos()
+
+
+def get_medicos():
+    dados = {
+        "nome": "Kaio Oliveira",
+        "crm": "123456",
+        "especialidade": "cardiologia",
+        "turno": "noturno",
+        "status": "ativo",
+    }
+
+    """
+        Nome	CRM	Especialidade	Turno	Situação	 
+        Dileyciane Monteiro	234567	Dermatologia	Diurno	Em análise	 
+        Naelle Monteiro	345678	Pediatria	Diurno	Ativo	 
+        Lidia Monteiro	456789	Nutrição	Vespertino	Inativo
+    """
+    return dados
