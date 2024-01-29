ce0dd67dadda87cd58f9cc6e8db4ea2eac19e30a
Luciana Monteiro Lima <dra.luciana.lima@gmail.com>
2024-01-29 13:48:03 -0300

    Exibir os dados usando template jinja

--- main.py
+++ main.py
@@ -1,46 +1,74 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 app.mount("/app", StaticFiles(directory="static", html="True"), name="static")
 
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=RedirectResponse)
 async def root():
     return "/app/login.html"
 
 
-@app.get("/api", response_class=HTMLResponse)
-async def api():
-    return """
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <meta http-equiv="refresh" content="0; URL='./app/login.html'">
-             </head>
-        </html>
-    """
+@app.get("/api", response_class=RedirectResponse)
+async def root():
+    return "/app/login.html"
 
+@app.get("/api/medicos", response_class=HTMLResponse)
+async def medicos(request: Request):
+    medicos = get_medicos()
+    dados_html = templates.TemplateResponse(
+        "tpl_medicos.html", {"request": request, "dados": medicos}
+    )
 
-@app.get("/api/medicos")
-async def medicos():
-    return get_medicos()
+    return dados_html
 
+# @app.get("/api/medicos", response_class=HTMLResponse)
+# async def medicos():
+#     dados_httml = """
+#         <table>
+#             <thead>
+#                 <tr>
+#                     <th>Nome</th>
+#                 </tr>
+#             </thead>
 
+#             <tbody>
+#                 <tr>
+#                     <td>Kaio Oliveira</td>
+#                 </tr>
+#             </tbody>
+#         </table>
+# """
+#     return dados_httml
+
+ 
 def get_medicos():
-    dados = {
-        "nome": "Kaio Oliveira",
-        "crm": "123456",
-        "especialidade": "cardiologia",
-        "turno": "noturno",
-        "status": "ativo",
-    }
+    dados = [
+        {
+            "nome": "Kaio Oliveira",
+            "crm": "123456",
+            "especialidade": "cardiologia",
+            "turno": "noturno",
+            "status": "ativo",
+        },
+        {
+            "nome": "Dileyciane Monteiro",
+            "crm": "234567",
+            "especialidade": "dermatologia",
+            "turno": "diurno",
+            "status": "em análise",
+        },
+    ]
+
+    return dados
 
     """
         Nome	CRM	Especialidade	Turno	Situação	 
-        Dileyciane Monteiro	234567	Dermatologia	Diurno	Em análise	 
+        					 
         Naelle Monteiro	345678	Pediatria	Diurno	Ativo	 
         Lidia Monteiro	456789	Nutrição	Vespertino	Inativo
     """
-    return dados
