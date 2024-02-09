1ccdc645ab2fe8a4487c5efd763aa02e754b87a4
Luciana Monteiro Lima <dra.luciana.lima@gmail.com>
2024-02-09 15:58:28 -0300

    Corrigir formato de retorno dos dados

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import RedirectResponse, HTMLResponse
-from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse, RedirectResponse, HTMLResponse
 
-import db 
+import db
 
 app = FastAPI()
 app.mount("/app", StaticFiles(directory="static", html="True"), name="static")
-
-templates = Jinja2Templates(directory="templates")
-
 
 @app.get("/", response_class=RedirectResponse)
 async def root():
@@ -23,13 +19,13 @@ async def root():
 
 @app.get("/api/pacientes")
 async def pacientes():
-    return get_pacientes()
+    dados = db.get_pacientes()
+    return JSONResponse(dados)
 
 
-@app.get("/api/medicos")
+@app.get("/api/medicos", response_class=JSONResponse)
 async def medicos():
-    dados = get_medicos()
-    return dados
+    return db.get_medicos()
 
 @app.delete("/api/medicos/{id}", response_class=HTMLResponse)
 async def medicos(id: str):
@@ -57,73 +53,3 @@ async def pacientes(id: str):
 #         </table>
 # """
 #     return dados_httml
-
-
-def get_medicos():
-    dados = [
-        {
-            "nome": "Kaio Oliveira",
-            "crm": "123456",
-            "email": "kaio@gmail.com",
-            "especialidade": "cardiologia",
-            "turno": "noturno",
-            "status": "ativo",
-        },
-        {
-            "nome": "Dileyciane Monteiro",
-            "crm": "234567",
-            "email": "ciane@gmail.com",
-            "especialidade": "dermatologia",
-            "turno": "diurno",
-            "status": "em análise",
-        },
-        {
-            "nome": "Naelle Monteiro",
-            "crm": "345678",
-            "email": "NAELLE@gmail.com",
-            "especialidade": "pediatria",
-            "turno": "DIURNO",
-            "status": "ATIVO",
-        },
-        {
-            "nome": "Lidia Monteiro",
-            "crm": "456789",
-            "email": "lidia@gmail.com",
-            "especialidade": "nutrição",
-            "turno": "vespertino",
-            "status": "INATIVO",
-        },
-    ]
-
-    return dados
-
-
-def get_pacientes():
-    dados = [
-        {
-            "nome": "Izaias Lima",
-            "email": "izaias@lima.com",
-            "telefone": "61 98181-3390",
-            "status": "internado",
-        },
-        {
-            "nome": "Luciete Lima",
-            "email": "luciete@lima.com",
-            "telefone": "61 98136-0050",
-            "status": "em atendimento",
-        },
-        {
-            "nome": "Natan Monteiro",
-            "email": "natan@monteiro.com",
-            "telefone": "61 98313-4298",
-            "status": "atendido",
-        },
-        {
-            "nome": "Luciana Monteiro",
-            "email": "luciana@monteiro.com",
-            "telefone": "61 98313-4289",
-            "status": "liberada",
-        },
-    ]
-
-    return dados
