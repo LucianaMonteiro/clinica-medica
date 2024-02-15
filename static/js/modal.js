fd5fff720208d685cc17aa95391f60c95d864abe
Luciana Monteiro Lima <dra.luciana.lima@gmail.com>
2024-02-15 16:10:10 -0300

    Adicionar modal de confirmacao animado

---
+++ static/js/modal.js
@@ -0,0 +1,20 @@
+document.addEventListener(
+    "htmx:confirm",
+    function (e) {
+        if (e.detail.question !== null) {
+            e.preventDefault();
+            Swal.fire({
+                icon:'question',
+                buttonsStyling: false,
+                showCancelButton: true,
+                reverseButtons: true,
+                title: 'Favor confirmar!',
+                text: `Deseja mesmo excluir o cadastro de ${(e.detail.question).toUpperCase()}?`,
+                showClass: { popup: 'animate__animated animate__fadeInUp animate_faster' },
+                hideClass: { popup: 'animate__animated animate__fadeOutDown animate_faster' },
+            }).then(function (res) {
+                if (res.isConfirmed) e.detail.issueRequest(true)
+            })
+        }
+    }
+);
