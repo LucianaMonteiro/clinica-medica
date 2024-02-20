1e55dbcf02fad17a3582cceb67cb51bbe1a474f5
Luciana Monteiro Lima <dra.luciana.lima@gmail.com>
2024-02-20 14:19:10 -0300

    Adicionar endpoint para editar paciente

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import JSONResponse, RedirectResponse, HTMLResponse
 
+import json
+import urllib.parse as html
+
 import db
 import db_init
+import static.fragments.html_edit as edit
 
 app = FastAPI()
 app.mount("/app", StaticFiles(directory="static", html="True"), name="static")
+
+async def get_body(req: Request):
+    payload = await req.body()
+    payload = payload.decode("utf8")
+    payload = html.unquote(payload)
+
+    try:
+        body = json.loads(payload)
+    except:
+        lista = list(payload.split("&"))
+        body = dict(l.split("=") for l in lista)
+
+    return body
 
 @app.get("/", response_class=RedirectResponse)
 async def root():
@@ -23,6 +40,21 @@ async def pacientes():
     dados = db.get_pacientes()
     return JSONResponse(dados)
 
+@app.get("/api/pacientes/{id}")
+async def pacientes(id):
+    dados = db.get_paciente(id)
+    return JSONResponse(dados)
+
+@app.put("/api/pacientes/{id}", response_class=JSONResponse)
+async def update_pacientes(id, body=Depends(get_body)):
+    db.update_pacientes(id, body)
+    dados = db.get_pacientes()
+    return dados
+
+@app.delete("/api/pacientes/{id}", response_class=HTMLResponse)
+async def paciente(id: str):
+    db.del_paciente(id)
+    return ""
 
 @app.get("/api/medicos", response_class=JSONResponse)
 async def medicos():
@@ -33,15 +65,22 @@ async def medico(id: str):
     db.del_medico(id)
     return ""
 
-@app.delete("/api/pacientes/{id}", response_class=HTMLResponse)
-async def paciente(id: str):
-    db.del_paciente(id)
-    return ""
-
+# resetar o banco de dados:
 @app.get("/reset", response_class=RedirectResponse)
 def db_reset():
     db_init.tables_init()
     return "/app/home.html"
+
+# retornar template para editar paciente:
+@app.get("/api/pacientes/{id}/edit", response_class=HTMLResponse)
+async def edit_paciente(id):
+    paciente = db.get_paciente(id)
+
+    if paciente:
+        dados = paciente[0]
+        return edit.paciente_html(dados)
+    else:
+        return ""
 
 # @app.get("/api/medicos", response_class=HTMLResponse)
 # async def medicos():
